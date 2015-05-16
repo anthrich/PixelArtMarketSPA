cbf299cc4c160106e8c8f3230b49342356f44ca4
anthrich83 <anthrblahblah@gmail.com>
2015-05-16 11:44:57 +0100

    MockProductService

---
+++ src/services/MockProductService.js
@@ -0,0 +1,22 @@
+﻿define(['../models/ProductModel'], function (ProductModel) {
+    function MockProductService() {
+    }
+
+    MockProductService.prototype.getProducts = function (skip, take) {
+        var products = [];
+        // TODO: Use services to get this shit
+        for (var i = skip; i < take + skip; i++) {
+            products.push(new ProductModel({
+                id: i,
+                name: "Product " + i,
+                price: "£" + Math.floor((Math.random() * 40) + 1),
+                imgUrl: "img/placeholder_png.jpg",
+                description: "Random desc for product " + i
+            }));
+        };
+
+        return products;
+    };
+
+    return MockProductService;
+});
